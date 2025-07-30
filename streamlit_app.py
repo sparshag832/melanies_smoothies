910be973b44960c52be00cc970d999ea5ccc4e19
Sparsh Agarwal <84582301+sparshag832@users.noreply.github.com>
2025-07-31 00:46:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,9 @@ from snowflake.snowpark.functions import col
 import requests
 
 # Write directly to the app
-st.title(f"This Is My First Streamlit App :cup_with_straw: {st.__version__}")
-st.write(
-  """Replace this example with your own code!
-  **And if you're new to Streamlit,** check
-  out our easy-to-follow guides at
-  [docs.streamlit.io](https://docs.streamlit.io).
-  """
-)
+st.title(f"Customize Your Smoothie!:cup_with_straw: {st.__version__}")
+
+st.write("Choose The Fuits For Your Custom Smoothie")
 
 name_on_order=st.text_input('Name On Smoothie')
 st.write("The name on your Smoothie will be",name_on_order)
@@ -27,6 +22,7 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen+' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
 
